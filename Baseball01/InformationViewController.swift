76d06ce4e6d6148b60a5b4c5a79fe1f86d4e8117
kaori <qbs0003@icloud.com>
2020-06-04 20:53:48 +0900

    Mapやアイコンの修正

--- Baseball01/InformationViewController.swift
+++ Baseball01/InformationViewController.swift
@@ -9,28 +9,86 @@
 import UIKit
 //MapKitのインポート
 import MapKit
+import CoreLocation
 
 
-class InformationViewController: UIViewController {
+class InformationViewController: UIViewController, CLLocationManagerDelegate {
 
+    // CLLocationManagerのメンバ変数を定義する
+    @IBOutlet var mapView: MKMapView!
+    var locManager: CLLocationManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        //MapViewを生成し、表示する
-        let myMapView = MKMapView()
-        myMapView.frame = self.view.frame
-        self.view.addSubview(myMapView)
+        // delegateを登録する
+        locManager = CLLocationManager()
+        locManager.delegate = self
+            
+        // 位置情報の使用の許可を得る
+        locManager.requestWhenInUseAuthorization()
+        if CLLocationManager.locationServicesEnabled() {
+            switch CLLocationManager.authorizationStatus() {
+            case .authorizedWhenInUse:
+                // 座標の表示
+                locManager.startUpdatingLocation()
+                break
+            default:
+                break
+            }
+        }
+            
+        // --------------------- 現在位置を中心にする処理関連 --------------------------
+        // 地図の初期化
+        //initMap()
+        // --------------------- 現在位置を中心にする処理関連 --------------------------
         
-        //長押しを探知する機能を追加
+        //---------------------- 長押しを探知する機能を追加 ----------------------------
         //ジェスチャーの生成
-        let longPressGesture = UILongPressGestureRecognizer()
+        //let longPressGesture = UILongPressGestureRecognizer()
         //ボタンを押したときの処理
-        longPressGesture.addTarget(self, action: "longPressed:")
-        myMapView.addGestureRecognizer(longPressGesture)
+        //longPressGesture.addTarget(self, action: "longPressed:")
+        //myMapView.addGestureRecognizer(longPressGesture)
+        //---------------------- 長押しを探知する機能を追加 ----------------------------
         
         // 背景に表示したい画像の名前(拡張子含む)を引数とする。
         //self.view.addBackground(name: "ground.jpg")
-        
     }
+        
+    // CLLocationManagerのdelegate：現在位置取得
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations:[CLLocation]) {
+        let lonStr = (locations.last?.coordinate.longitude.description)!
+        let latStr = (locations.last?.coordinate.latitude.description)!
+
+        print("lon : " + lonStr)
+        print("lat : " + latStr)
+            
+        // --------------------- 現在位置を中心にする処理関連 --------------------------
+        //mapView.userTrackingMode = .follow
+        // --------------------- 現在位置を中心にする処理関連 --------------------------
+    }
+        
+    // --------------------- 現在位置を中心にする処理関連 --------------------------
+    // 地図の初期化関数
+    func initMap() {
+        // 縮尺を設定
+        var region:MKCoordinateRegion = mapView.region
+        region.span.latitudeDelta = 0.02
+        region.span.longitudeDelta = 0.02
+        mapView.setRegion(region,animated:true)
+
+        // 現在位置表示の有効化
+        mapView.showsUserLocation = true
+        // 現在位置設定（デバイスの動きとしてこの時の一回だけ中心位置が現在位置で更新される）
+        mapView.userTrackingMode = .follow
+    }
+        
+    //地図の中心位置の更新関数
+    func updateCurrentPos(_ coordinate:CLLocationCoordinate2D) {
+        var region:MKCoordinateRegion = mapView.region
+        region.center = coordinate
+        mapView.setRegion(region,animated:true)
+    }
+    // --------------------- 現在位置を中心にする処理関連 --------------------------
 
 }
